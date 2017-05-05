48d18ef33e97a7ea1df1a05f04c9ea41b7a01aea
wc563388548@gmail.com <wc563388548@gmail.com>
2017-05-05 21:17:39 +0900

    Using ViewHolder and convertView of getView() method to optimize ListView

--- app/src/main/java/com/example/wwk/thetokyo/ContentsAdapter.java
+++ app/src/main/java/com/example/wwk/thetokyo/ContentsAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -29,63 +30,78 @@ public class ContentsAdapter extends ArrayAdapter<Contents> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
-        // Check if an existing view is being reused, otherwise inflate the view
-        View listItemView = convertView;
-        if (listItemView == null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
-        }
-
         // Get the {@link contents} object located this position in the list
         Contents currentCotents = getItem(position);
 
-        ImageView displayImage = (ImageView) listItemView.findViewById(R.id.image_view);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        View listItemView;
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.textTainer = (LinearLayout) listItemView.findViewById(R.id.text_container);
+            viewHolder.spotImage = (ImageView) listItemView.findViewById(R.id.image_view);
+            viewHolder.spotName = (TextView) listItemView.findViewById(R.id.spotname_text_view);
+            viewHolder.spotAddress = (TextView) listItemView.findViewById(R.id.spotaddress_text_view);
+            listItemView.setTag(viewHolder);
+        } else {
+            listItemView = convertView;
+            viewHolder = (ViewHolder) listItemView.getTag();
+        }
+
+        //ImageView displayImage = (ImageView) listItemView.findViewById(R.id.image_view);
         // Check if an image is provided for this word or not
-        if (currentCotents.hasImage()) {
+        //if (currentCotents.hasImage()) {
 
             // If an image is available, display the provided image based on the resource ID
-            displayImage.setImageResource(currentCotents.getmImageResourceId());
+            //displayImage.setImageResource(currentCotents.getmImageResourceId());
 
             // Make sure the view is visible
-            displayImage.setVisibility(View.VISIBLE);
-        } else {
+            //displayImage.setVisibility(View.VISIBLE);
+        //} else {
 
             // Otherwise hide the ImageView (set visibility to GONE)
-            displayImage.setVisibility(View.GONE);
-        }
+            //displayImage.setVisibility(View.GONE);
+        //}
 
 
         // Set the theme color for the list item
-        View textcontainer = listItemView.findViewById(R.id.text_container);
+        //View textcontainer = listItemView.findViewById(R.id.text_container);
 
         // Find the color that the resource ID maps to
-        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        //int color = ContextCompat.getColor(getContext(), mColorResourceId);
 
         // Set the background color of the text container View
-        textcontainer.setBackgroundColor(color);
+        //textcontainer.setBackgroundColor(color);
 
 
         // Find the TextView in the list_item.xml layout with the ID japanese_text_view.
-        TextView spotNameTextView = (TextView) listItemView.findViewById(R.id.spotname_text_view);
+        //TextView spotNameTextView = (TextView) listItemView.findViewById(R.id.spotname_text_view);
 
         // Get the Japanese translation from the currentWords object and set this text
         // on the TextView
-        spotNameTextView.setText(currentCotents.getmSpotsName());
+        //spotNameTextView.setText(currentCotents.getmSpotsName());
 
-        TextView spotAddressTextView = (TextView) listItemView.findViewById(R.id.spotaddress_text_view);
-        spotAddressTextView.setText(currentCotents.getmSpotsAddress());
+        //TextView spotAddressTextView = (TextView) listItemView.findViewById(R.id.spotaddress_text_view);
+        //spotAddressTextView.setText(currentCotents.getmSpotsAddress());
 
+        viewHolder.textTainer.setBackgroundColor(color);
+        viewHolder.spotImage.setImageResource(currentCotents.getmImageResourceId());
+        viewHolder.spotName.setText(currentCotents.getmSpotsName());
+        viewHolder.spotAddress.setText(currentCotents.getmSpotsAddress());
         // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
         // the ListView.
         return listItemView;
 
     }
 
-    //class ViewHolder {
+    private class ViewHolder {
+        LinearLayout textTainer;
+        ImageView spotImage;
 
-    //  ImageView spotImage;
-
-    //TextView spotName;
-    //    TextView spotAddress;
-    //}
+        TextView spotName;
+        TextView spotAddress;
+    }
 }
